ccabbd1f1b34e1def48bdce8bd6515d8a7d68d6d
Ryan Schuster <rjs48792@uga.edu>
2021-02-06 01:00:41 -0500

    added stuff

--- main.py
+++ main.py
@@ -17,8 +17,15 @@ def main():
 
     DISPLAY.fill(WHITE)
 
-    pygame.draw.rect(DISPLAY,(170, 170, 170),(0,0,infoObject.current_w, infoObject.current_h), 100)
-
+    #pygame.draw.rect(DISPLAY,(170, 170, 170),(0,0,infoObject.current_w, infoObject.current_h), 100)
+    randomSingleGazeShape(DISPLAY, infoObject, 100, 100)
+    pygame.draw.rect(DISPLAY, (0,0,0),(0,0, 100,100), 1)
+    """randomSingleGazeShape(DISPLAY, 100, 200)
+    randomSingleGazeShape(DISPLAY, 230, 400)
+    randomSingleGazeShape(DISPLAY, 600, 60)
+    randomSingleGazeShape(DISPLAY, 800, 40)"""
+    pygame.draw.rect(DISPLAY, (0,0,0),(0,0 ,1000,600), 1)
+    randomSingleGazeShape(DISPLAY, infoObject, 1000, 600)
     while True:
         for event in pygame.event.get():
             if event.type==QUIT: #implemt a way to quit, with eyes maybe
@@ -28,28 +35,64 @@ def main():
 
 
 
-def randomSingleGazeShape(x, y):
-    ranShape = random.randint(0,5)
+def randomSingleGazeShape(DISPLAY, infoObject, x, y):
+    ranShape = random.randint(0,0) #change back
     ranRed = random.randint(0, 255)
     ranGreen = random.randint(0, 255)
     ranBlue = random.randint(0, 255)
     ranThick = random.randint(0, 15)
-    ranW = random.ranint(5, 50)
-    ranH = random.ranint(5, 50)
-    if (ranShape == 0): #line
-        print("tex")
-    elif (ranShape == 1): #circle
-        print("tex")
+    ranW = random.randint(5, 50)
+    ranH = random.randint(5, 50)
+    ranStartX = random.randint(x - 100, x + 100)
+    ranStartY = random.randint(y - 100, y + 100)
+    ranEndX = random.randint(x - 100, x + 100)
+    ranEndY = random.randint(y - 100, y + 100)
+    ranRadius = random.randint(1, 40)
+
+    ranBool = random.randint(0,3)
+    if (ranBool == 0):
+        longRanStartX = random.randint(0, x)
+        longRanEndX = x
+        longRanStartY = random.randint(0, y)
+        longRanEndY = y
+    elif (ranBool == 1):
+        longRanStartX = random.randint(x, infoObject.current_w)
+        longRanEndX = x
+        longRanStartY = random.randint(y, infoObject.current_h)
+        longRanEndY = y 
+    elif (ranBool == 2):
+        longRanStartX = random.randint(0, x)
+        longRanEndX = x
+        longRanStartY = random.randint(y, infoObject.current_h)
+        longRanEndY = y 
+    elif (ranBool == 3):
+        longRanStartX = random.randint(x, infoObject.current_w)
+        longRanEndX = x
+        longRanStartY = random.randint(0, y)
+        longRanEndY = y 
+
+
+    if (ranShape == 0): #shortline single or triple
+        ranBool = random.randint(0,1)
+        if (ranBool == 0):
+            pygame.draw.line(DISPLAY, (ranRed, ranGreen, ranBlue), (ranStartX, ranStartY), (ranEndX, ranEndY), ranThick)
+        elif (ranBool == 1):
+            pygame.draw.line(DISPLAY, (ranRed, ranGreen, ranBlue), (ranStartX, ranStartY), (ranEndX, ranEndY), ranThick)
+            pygame.draw.line(DISPLAY, (ranRed, ranGreen, ranBlue), (ranStartX - 20, ranStartY - 20), (ranEndX - 20, ranEndY - 20), ranThick)
+            pygame.draw.line(DISPLAY, (ranRed, ranGreen, ranBlue), (ranStartX + 20, ranStartY + 20), (ranEndX + 20, ranEndY + 20), ranThick)
+    elif (ranShape == 1): #longline
+        pygame.draw.line(DISPLAY, (ranRed, ranGreen, ranBlue), (longRanStartX, longRanStartY), (longRanEndX, longRanEndY), ranThick)
     elif (ranShape == 2): #rectangle
         pygame.draw.rect(DISPLAY, (ranRed, ranGreen, ranBlue),(x,y, ranW, ranH), ranThick)
     elif (ranShape == 3): #triangle
-        print("tex")
+        pygame.draw.polygon(DISPLAY, (ranRed, ranGreen, ranBlue), ((x - 15, x - 15), (y + 25, y - 15), (y - 15, y - 15)), ranThick)
     elif (ranShape == 4): #arc
         print("tex")
     elif (ranShape == 5): #polygons
         print("tex")
-
-
+    elif (ranShape == 6): #circle
+        pygame.draw.circle(DISPLAY, (ranRed, ranGreen, ranBlue),(x,y), ranRadius, ranThick)
+        
 def randomDoubleGazeShape(x1, y1, x2, y2):
     ranShape = random.randint(0,5)
     if (ranShape == 0): #line
